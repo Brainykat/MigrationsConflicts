cdd56d9f4c39f2db9629c3350d724b551c5a54fa
Chris <eng.christhuku@gmail.com>
2022-04-05 10:49:01 +0300

    add feature A

---
+++ MigrationsConflicts/Migrations/20220405074822_feature_A.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MigrationsConflicts.Migrations
+{
+    public partial class feature_A : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Field_A",
+                table: "IOTAssets",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Field_A",
+                table: "IOTAssets");
+        }
+    }
+}
